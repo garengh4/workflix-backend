ba022b0f464c7af2941ba43d07435095e425b822
garengh4 <garengh4@gmail.com>
2023-02-23 17:40:18 -0500

    init master commit

---
+++ src/main/java/com/infy/workflixbackend/WorkflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.infy.workflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/workflixbackend/WorkflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.workflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
