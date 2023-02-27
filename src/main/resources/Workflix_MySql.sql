dd3731232c91a6c11ff9bfed59411b672e791193
Sharon Voon <sharon.voon@infosys.com>
2023-02-27 11:40:15 -0800

    altered table in the sql database

--- src/main/resources/Workflix_MySql.sql
+++ src/main/resources/Workflix_MySql.sql
@@ -3,23 +3,16 @@ use workflix;
 
 CREATE TABLE email(
 	email_id VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(20) NOT NULL,
+    passwords VARCHAR(20) NOT NULL);
 
 CREATE TABLE user_profile (
 	user_profile_id VARCHAR(50) PRIMARY KEY,
 	FOREIGN KEY (email_id) REFERENCES email(email_id),
     user_first_name VARCHAR(50) NOT NULL,
     user_last_name VARCHAR(50) NOT NULL);
---     user_gender VARCHAR(3) NOT NULL,
---     phone_number LONG UNIQUE,
---     user_dob DATE NOT NULL);
 
--- CREATE TABLE workflix_access(
--- 	FOREIGN KEY (user_id) REFERENCES workflix_user(user_id),
---     FOREIGN KEY (friend_id) REFERENCES workflix_user(user_id));
-    
 CREATE TABLE files(
-	file_id LONG AUTO_INCREMENT PRIMARY KEY,
+	file_id LONG PRIMARY KEY,
 	file_name VARCHAR(50) NOT NULL,
 	FOREIGN KEY (user_profile_id) REFERENCES user_profile (user_profile_id),
 	descriptions VARCHAR(50),
@@ -31,4 +24,8 @@ CREATE TABLE files(
 --     FOREIGN KEY (comment_file) REFERENCES upload(file_id),
 --     FOREIGN KEY (comment_user) REFERENCES user_profile(user_name));
 
+
+
+INSERT INTO email (email_id, passwords) VALUES ('janeDoe@example.com', '123456789');
+INSERT INTO email (email_id, passwords) VALUES ('johnSmith@example.com', '123456789');
     
