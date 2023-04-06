3a035db1df441e1bfd340db6b9aa5ca5ca4046b7
Garen <garengh4@gmail.com>
2023-04-06 19:00:48 -0400

    safe revert here

--- src/main/java/com/infy/workflixbackend42/WorkflixBackend42Application.java
+++ src/main/java/com/infy/workflixbackend42/WorkflixBackend42Application.java
@@ -2,10 +2,11 @@ package com.infy.workflixbackend42;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
-import java.util.List;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:messages.properties" })
 public class WorkflixBackend42Application {
 
 	public static void main(String[] args) {
