d9e463ef635e1132612d98cfcbf23a130025007e
garengh4 <garengh4@gmail.com>
2023-04-11 13:18:21 -0400

    removed employee

--- src/main/java/com/infy/workflixbackend42/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.infy.workflixbackend42;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {}
