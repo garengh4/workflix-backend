431a2f4b6bde642e4cb33465aaa04e12f0448000
Gareth Sykes <garethcsykes@gmail.com>
2023-04-13 09:43:39 -0400

    changes to pom.xml and securityConfig for Swagger

--- src/main/java/com/infy/workflixbackend2/security/SecurityConfig.java
+++ src/main/java/com/infy/workflixbackend2/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.infy.workflixbackend2.security;
 import com.infy.workflixbackend2.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import javax.servlet.http.HttpServletResponse;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -24,6 +27,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtTokenFilter jwtTokenFilter;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**",
+                "/configuration/ui",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**",
+                "/swagger-resources/configuration/ui",
+                "/swagger-ui.html",
+                "/swagger-ui/**");
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
